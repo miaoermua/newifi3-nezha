e28d8317510e06e3c2ddb44f59672b7c7e03db64
喵二 <2860950766@qq.com>
2022-12-12 19:43:37 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-ping github.com -c 1
-echo "设备正常访问 Github，正在下载配置"
+echo "部署初始环境"
 wget -P /root https://github.com/miaoermua/newifi3-nezha/raw/main/down.sh
 wget -P /etc/init.d https://github.com/miaoermua/newifi3-nezha/raw/main/nezha-service
 wget -P /tmp https://github.com/miaoermua/newifi3-nezha/raw/main/nezha-agent
 chmod +x /root/down.sh
 chmod +x /etc/init.d/nezha-service
 chmod +x /tmp/nezha-agent
-echo "部署完毕,为了安全已抹去隐私信息"
+echo "部署完毕"
 vi /etc/init.d/nezha-service
